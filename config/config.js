c6d47f36d5320a6afc0c2a9539ef914ec621b103
anbuk143 <anbukumar1913@gmail.com>
2025-01-03 20:52:18 +0530

    Final

--- config/config.js
+++ config/config.js
@@ -2,5 +2,3 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.connect(process.env.MONGOURL)
     .then(() => console.log('MongoDB Connected!')).catch((error) => console.log(error))
-
-    // "mongodb+srv://anbukumar1913:b62g2nlPG5aHByKv@collection.xfyzh.mongodb.net/userCollection"
